1882a2cf664b2af77af22efaf7509e51de0f775e
Jay Asbury <vbjay.net@gmail.com>
2019-02-12 19:39:52 -0500

    Add a log message when button is clicked

--- SerilogTester/frmTest.vb
+++ SerilogTester/frmTest.vb
@@ -1,4 +1,7 @@
+﻿Imports Serilog
+
 Public Class frmTest
 	 Private Sub btnTest_Click(sender As Object, e As EventArgs) Handles btnTest.Click
+		  Log.Logger.Information("You clicked me.")
 	 End Sub
 End Class

7bafac3b1474051ff1b0de0382a55d88295ed58f
Jay Asbury <vbjay.net@gmail.com>
2019-02-12 19:37:49 -0500

    Add  basic log configuration

---
+++ SerilogTester/ApplicationEvents.vb
@@ -0,0 +1,21 @@
+﻿Imports Microsoft.VisualBasic.ApplicationServices
+Imports Serilog
+
+Namespace My
+	 ' The following events are available for MyApplication:
+	 ' Startup: Raised when the application starts, before the startup form is created.
+	 ' Shutdown: Raised after all application forms are closed.  This event is not raised if the application terminates abnormally.
+	 ' UnhandledException: Raised if the application encounters an unhandled exception.
+	 ' StartupNextInstance: Raised when launching a single-instance application and the application is already active. 
+	 ' NetworkAvailabilityChanged: Raised when the network connection is connected or disconnected.
+	 Partial Friend Class MyApplication
+		  Private Sub MyApplication_Startup(sender As Object, e As StartupEventArgs) Handles Me.Startup
+				Serilog.Log.Logger = New LoggerConfiguration().
+					 MinimumLevel.Debug().
+					 Enrich.FromLogContext.
+					 CreateLogger()
+				Serilog.Log.Logger.Information("App start.")
+
+		  End Sub
+	 End Class
+End Namespace
